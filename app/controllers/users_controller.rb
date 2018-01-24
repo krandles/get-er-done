a1c52ac979de66d0f6e9a2dc28b2a340686f4718
github username <github email address>
2018-01-24 16:29:41 -0500

    Added Experience for Projects

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,7 +50,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :user_name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:first_name, :last_name, :user_name, :email, :password, :password_confirmation, :experience)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :user_projects
   has_many :projects, through: :user_projects
   has_many :tasks
-  
+
   has_secure_password
   validates :user_name, :email, uniqueness: true
   validates :user_name, :email, :first_name, :last_name, :password, :password_confirmation, presence: true
